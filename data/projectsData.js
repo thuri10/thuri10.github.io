8bd2c5058f97443a3e8977a07bcf041263a2cd7a
thuri10 <thuri783@gmail.com>
2022-02-13 22:56:49 +0300

    Fix images

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,3 +1,5 @@
+const prefix = "/nestjs-blog"
+
 const projectsData = [
   {
     title: 'A Search Engine',
@@ -12,7 +14,7 @@ const projectsData = [
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: prefix + '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
 ]
