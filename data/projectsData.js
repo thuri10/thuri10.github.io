c883450814198741acad711e1320bbf9eebd3d0a
thuri10 <thuri783@gmail.com>
2022-02-13 23:01:31 +0300

    Fix prefixes

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,7 +6,7 @@ const projectsData = [
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: prefix + '/static/images/google.png',
     href: 'https://www.google.com',
   },
   {
@@ -15,7 +15,7 @@ const projectsData = [
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: prefix + '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: prefix + '/blog/the-time-machine',
   },
 ]
 
