0c0d7ae5763cbf840fad23f9cc20112beaeeb17f
thuri10 <thuri783@gmail.com>
2022-02-14 22:13:05 +0300

    Remove mdx from file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack')
 
 
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
