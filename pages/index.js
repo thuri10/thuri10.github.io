cae9a9353fd4d27e802dd4bbb3fee4dceb107e8c
thuri10 <thuri783@gmail.com>
2022-07-26 17:40:54 +0300

    Update Deployment Workflow

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,10 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
-import Hero from '@/components/Hero'
+import IMage from '@/components/Image'
+import NewsletterForm from '@/components/NewsletterForm'
 
-const MAX_DISPLAY = 6
+const MAX_DISPLAY = 5
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
@@ -18,63 +19,81 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <Hero />
-      <div className="container mx-auto divide-y divide-gray-700">
-        <div className="my-4 flex flex-col">
-          <span className="font-poppins title-font  text-3xl font-bold">Recent Posts</span>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
         </div>
-        <div className="grid grid-cols-1 gap-8 pt-10 md:grid-cols-2 xl:grid-cols-3">
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags, images } = frontMatter
-            const firstTwoTags = tags.slice(0, 1)
+            const { slug, date, title, summary, tags } = frontMatter
             return (
-              <div
-                key={slug}
-                className="bg-day group relative h-full transform rounded-lg transition duration-500 hover:scale-105"
-              >
-                <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  opacity-0 blur transition duration-1000 group-hover:opacity-20 group-hover:duration-200"></div>
-                <a className="c-card bg-cardBg relative block h-full overflow-hidden rounded-lg shadow-none">
-                  <div className="group relative max-h-4 overflow-hidden rounded-lg pb-60">
-                    <Link href={`/blog/${slug}`}>
-                      <span>
-                        <img
-                          alt={title}
-                          src={images}
-                          className="absolute inset-0 h-full w-full  object-cover shadow-none "
-                        />
-                      </span>
-                    </Link>
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                  <div className="h-full py-4 px-2">
-                    <span className="inline-flex w-full items-center justify-between">
-                      <span className="inline-block rounded-full border border-green-700 py-1 px-2 text-xs font-medium">
-                        {firstTwoTags.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </span>
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </span>
-                    <h2 className="mt-2 mb-2 font-bold md:text-xl">
-                      <Link href={`/blog/${slug}`} className="text-white-100">
-                        {title}
-                      </Link>
-                    </h2>
-                    <p className="h-auto text-sm tracking-wider">{summary}</p>
-                  </div>
-                </a>
-              </div>
+                </article>
+              </li>
             )
           })}
-        </div>
-        {posts.length > MAX_DISPLAY && (
-          <div className="mt-5 flex justify-end text-base font-medium leading-6">
-            <Link href="/blog" className="mt-5 hover:text-primary-400" aria-label="all posts">
-              All Posts &rarr;
-            </Link>
-          </div>
-        )}
+        </ul>
       </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider !== '' && (
+        <div className="flex items-center justify-center pt-4"></div>
+      )}
     </>
   )
 }
